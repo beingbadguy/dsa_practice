16415cf0dbe4a6c8aac54af329dca08cd5aea240
Aman Kumar <authorisedaman@gmail.com>
2025-09-23 07:43:27 +0530

    max subarray sum

---
+++ max_subarray_product.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void max_subarray_product(vector<int> &nums)
+{
+    int maxProduct = nums[0];
+    int minProduct = nums[0];
+    int result = nums[0];
+
+    for (int k = 1; k < nums.size(); k++)
+    {
+        int i = nums[k];
+
+        if (i < 0)
+            swap(maxProduct, minProduct);
+
+        maxProduct = max(i, i * maxProduct);
+        minProduct = min(i, i * minProduct);
+
+        result = max(result, maxProduct);
+    }
+
+    cout << "The max subarray product is : " << result << endl;
+}
+
+int main()
+{
+    vector<int> binary = {2, 3, -2, 4};
+    max_subarray_product(binary);
+    return 0;
+}
